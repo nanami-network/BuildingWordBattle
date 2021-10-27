d99758dcf14cad09fc674d913dc82402c64771fc
tina-chan <kassi_0123@kassi-hp.info>
2021-10-28 00:09:33 +0900

    なんかDiscord系統追加

--- src/main/java/me/koutachan/buildingwordbattle/Game/DiscordManager.java
+++ src/main/java/me/koutachan/buildingwordbattle/Game/DiscordManager.java
@@ -1,9 +1,14 @@
 package me.koutachan.buildingwordbattle.Game;
 
+import me.koutachan.buildingwordbattle.Game.Discord.DiscordMessageEvent;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 public class DiscordManager extends ListenerAdapter {
 
@@ -24,5 +29,23 @@ public class DiscordManager extends ListenerAdapter {
         }
     }
 
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
 
+        new Thread(()->{
+            Plugin plugin = Bukkit.getPluginManager().getPlugin("BuildingWordBattle");
+
+            Bukkit.getScheduler().runTask(plugin, ()->{
+                DiscordMessageEvent e = new DiscordMessageEvent();
+
+                e.setTextChannelId(event.getTextChannel().getId());
+                e.setTextMessageId(event.getMessageId());
+                e.setMessage(event.getMessage().getContentDisplay());
+                e.setMemberId(event.getMember().getId());
+
+                plugin.getServer().getPluginManager().callEvent(e);
+            });
+        }).start();
+
+    }
 }
