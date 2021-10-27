8a0fc415f65a68ea9d144f79dde678a47cc45533
tina-chan <kassi_0123@kassi-hp.info>
2021-10-27 23:55:09 +0900

    add DiscordManager

--- src/main/java/me/koutachan/buildingwordbattle/Game/DiscordManager.java
+++ src/main/java/me/koutachan/buildingwordbattle/Game/DiscordManager.java
@@ -1,4 +1,28 @@
 package me.koutachan.buildingwordbattle.Game;
 
-public class DiscordManager {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class DiscordManager extends ListenerAdapter {
+
+    private JDA jda = null;
+
+    public DiscordManager(String token) throws Exception{
+        JDABuilder builder = JDABuilder.createDefault(token);
+        builder.addEventListeners(this);
+        jda = builder.build();
+    }
+
+    public void sendMessage(String TextChannelId, String Message) throws Exception{
+
+        TextChannel channel = jda.getTextChannelById(TextChannelId);
+
+        if (channel != null){
+            channel.sendMessage(Message).queue();
+        }
+    }
+
+
 }
