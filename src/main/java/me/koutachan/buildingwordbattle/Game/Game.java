43ec4ed356a4b5c3f14c8206fe09f5fc6bb8e6e5
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-10-29 01:03:54 +0900

    fix bug

--- src/main/java/me/koutachan/buildingwordbattle/Game/Game.java
+++ src/main/java/me/koutachan/buildingwordbattle/Game/Game.java
@@ -10,47 +10,81 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Game {
     public static void startShuffle() {
 
-        List<Player> onlinePlayers = new ArrayList<>(Bukkit.getOnlinePlayers());
-        Collections.shuffle(onlinePlayers);
 
-        for (Player player : Bukkit.getOnlinePlayers()) {
+        Map<UUID, Integer> shuffle = shuffleWhile();
 
-            PlayerData data = PlayerDataUtil.getPlayerData(player);
+        if (shuffle != null) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                PlayerData data = PlayerDataUtil.getPlayerData(player);
 
-            if (data.getTeamManager().getCurrentTeam() == TeamEnum.PLAYER) {
-                int foundNumber = 0;
+                if (data.getTeamManager().getCurrentTeam() == TeamEnum.PLAYER) {
 
-                List<Integer> cloneMapList = new ArrayList<>(GameInfo.mapList);
+                    int mapID = shuffle.get(player.getUniqueId());
 
-                List<Integer> playedMap = data.getMapManager().getMapList();
+                    data.getMapManager().addMap(mapID);
 
-                for (int i : cloneMapList) {
-                    if (!playedMap.contains(i)) {
-                        playedMap.add(i);
-                        foundNumber = i;
-                        break;
+                    AreaCreator areaCreator = CreateBox.areaCreatorMap.get(mapID + "-" + GameInfo.round);
+
+                    World world = Bukkit.getWorld("world");
+
+                    Vector vector = new Vector(areaCreator.getXMax(), areaCreator.getYMax(), areaCreator.getZMax()).getMidpoint(new Vector(areaCreator.getXMin(), areaCreator.getYMin(), areaCreator.getZMin()));
+
+                    player.teleport(new Location(world, vector.getX(), vector.getY(), vector.getZ()));
+                }
+            }
+        } else {
+            //ゲーム終了
+        }
+    }
+
+    private static Map<UUID, Integer> shuffleWhile() {
+        while (true) {
+            int temp = 0;
+
+            Map<UUID, Integer> hashmap = new HashMap<>();
+
+            List<Player> onlinePlayers = new ArrayList<>(Bukkit.getOnlinePlayers());
+            Collections.shuffle(onlinePlayers);
+
+            List<Integer> cloneMapList = new ArrayList<>(GameInfo.mapList);
+            for (Player player : Bukkit.getOnlinePlayers()) {
+
+                PlayerData data = PlayerDataUtil.getPlayerData(player);
+
+                if (data.getTeamManager().getCurrentTeam() == TeamEnum.PLAYER) {
+
+
+                    int foundNumber = 0;
+
+
+                    List<Integer> playedMap = data.getMapManager().getMapList();
+
+                    for (int i : cloneMapList) {
+                        if (!playedMap.contains(i)) {
+                            cloneMapList.remove((Integer) i);
+                            foundNumber = i;
+                            break;
+                        }
+                    }
+
+                    if (foundNumber == 0) {
+                        if (temp++ > 1) {
+                            return null;
+                        }
+                    }
+
+                    if (temp == 0) {
+                        hashmap.put(player.getUniqueId(), foundNumber);
                     }
                 }
-
-                if (foundNumber == 0) {
-                    Bukkit.broadcastMessage("end the GAME!");
-                    //end the game
-                }
-
-                AreaCreator areaCreator = CreateBox.areaCreatorMap.get(foundNumber + "-" + GameInfo.round);
-
-                World world = Bukkit.getWorld("world");
-
-                Vector vector = new Vector(areaCreator.getXMax(), areaCreator.getYMax(), areaCreator.getZMax()).getMidpoint(new Vector(areaCreator.getXMin(), areaCreator.getYMin(), areaCreator.getZMin()));
-
-                player.teleport(new Location(world, vector.getX(), vector.getY(), vector.getZ()));
+            }
+            if (temp == 0) {
+                return hashmap;
             }
         }
     }
